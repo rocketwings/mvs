77c42df6fbae794ba8493c74622216e264ab07e2
Winson <rocketwings@gmail.com>
2018-06-13 16:50:50 -0700

    gitignore and basic svm

---
+++ seperable.m
@@ -0,0 +1,24 @@
+clear all;
+X = [[0 0]', [1 1]', [1 0]'];   %data
+Y = [-1 1 1]; %labels
+
+num_data = size(X,2);   l=num_data;
+num_dim = size(X,1);    d=num_dim;
+
+cvx_begin
+
+variable w(d);
+variable b;
+dual variable alpha;
+
+minimize( norm(w) );
+
+subject to
+    alpha : ((Y' .* (X'*w + ones(l,1)*b)) - ones(l,1)) >= 0 ;
+    
+cvx_end
+
+
+
+
+
