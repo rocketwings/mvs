09edb2b931f43538e263d3cca7c7187f5bf284ee
Winson <rocketwings@gmail.com>
2018-06-13 17:28:11 -0700

    made separable_train into a function

--- separable_train.m
+++ separable_train.m
@@ -2,12 +2,16 @@ function [ output_args ] = separable_train( data, labels )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
+if nargin < 2
+    X = [[0 0]', [1 1]', [1 0]'];   %data
+    Y = [-1 1 1]; %labels
+else
+    X = data;
+    Y = labels;
+end
 
-X = [[0 0]', [1 1]', [1 0]'];   %data
-Y = [-1 1 1]; %labels
-
-num_data = size(X,2);   l=num_data;
-num_dim = size(X,1);    d=num_dim;
+num_data = size(X,2);   %l=num_data;
+num_dim = size(X,1);    %d=num_dim;
 
 cvx_begin
 
@@ -18,9 +22,13 @@ dual variable alpha;
 minimize( norm(w) );
 
 subject to
-    alpha : ((Y' .* (X'*w + ones(l,1)*b)) - ones(l,1)) >= 0 ;
+    alpha : ((Y' .* (X'*w + ones(num_data,1)*b)) - ones(num_data,1)) >= 0 ;
     
 cvx_end
 
-end
+output_args = struct();
+output_args.w = w;
+output_args.b = b;
+output_args.dual = alpha;
 
+end
